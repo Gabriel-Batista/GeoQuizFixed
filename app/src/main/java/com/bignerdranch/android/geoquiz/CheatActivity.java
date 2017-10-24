8e6ddf203720e89394d171bcbdb4b7ec82b9ceda
Gabriel Batista <gabedbatista@gmail.com>
2017-10-24 00:58:33 -0400

    Added logic to CheatActivity to receive Intent with extras so that the cheat button can display the right answer

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,13 +1,54 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
+    //Key for KEY-VALUE pair as a constant
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+
+    //Create a new method which creates an intent with the extra already attached
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue)    {
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+
+        return intent;
+    }
+
+    private boolean mAnswerIsTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        //Get the extra that was sent with question answer
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        //Create reference to answer text view
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        //Create reference to show answer button
+        mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+
+        //Create listener for button and display cheat.
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mAnswerIsTrue)   {
+                    mAnswerTextView.setText(R.string.true_button);
+                }else{
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 }
