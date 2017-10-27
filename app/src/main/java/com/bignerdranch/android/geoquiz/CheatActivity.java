3fa5b68eaeba21f45b93b0d13cc99cbb1e46ba36
LilEggz <gabedbatista@gmail.com>
2017-10-27 19:35:21 -0400

    Chapter 5: Your Second Activity Challenge Complete

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -4,13 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
-    //Key for KEY-VALUE pair as a constant
-    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";   //Key for KEY-VALUE pair as a constant
+    private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";       //Key for KEY-PAIR value which sends info to see if user cheated
+    private static final String TAG = "QuizActivity";                                                       //Creates a string which the log method calls upon to determine which class we are working with.
+    private static final String KEY_CHEATED = "cheated";
+
+    private boolean mCheater;
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
 
     //Create a new method which creates an intent with the extra already attached
     public static Intent newIntent(Context packageContext, boolean answerIsTrue)    {
@@ -20,10 +29,11 @@ public class CheatActivity extends AppCompatActivity {
         return intent;
     }
 
-    private boolean mAnswerIsTrue;
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
 
-    private TextView mAnswerTextView;
-    private Button mShowAnswerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +58,35 @@ public class CheatActivity extends AppCompatActivity {
                 }else{
                     mAnswerTextView.setText(R.string.false_button);
                 }
+
+                setAnswerShownResult(true);
             }
         });
+
+        if(savedInstanceState != null)   {
+            setAnswerShownResult(savedInstanceState.getBoolean(KEY_CHEATED, false));
+
+            if(mAnswerIsTrue)   {
+                mAnswerTextView.setText(R.string.true_button);
+            }else{
+                mAnswerTextView.setText(R.string.false_button);
+            }
+        }
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown)   {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+
+        mCheater = true;
+    }
+
+    //Saves mCurrentState in Bundle (set of key-value-pairs) so when screen rotates app doesnt reset.
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(KEY_CHEATED, mCheater);
     }
 }
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";   //Creates a string which the log method calls upon to determine which class we are working with.
     private static final String KEY_INDEX = "index";    //Creates key for key-value-pair allowing us to save state in bundle when rotating device.
+    private static final String KEY_CHEAT = "cheat";   //Creates key for key pair value allowing to save whether user cheated
+    private static final int REQUEST_CODE_CHEAT = 0;    //Creates a int which is sent to a child activity and received back to check who is sending it.
 
     private Button mTrueButton;             //Holds reference to True Button
     private Button mFalseButton;            //Holds reference to False Button
@@ -21,11 +24,10 @@ public class QuizActivity extends AppCompatActivity {
     private Button mCheatButton;            //Holds reference to Cheat Button
     private TextView mQuestionTextView;     //Holds reference to QuestionTextView ( the space where question is displayed )
 
-    private int mScore =0;                     //Keeps score for game.
-
+    private int mScore = 0;                     //Keeps score for game.
 
     //Creates an array of questions for the app to draw from
-    private Question[] mQuestionBank = new Question[]   {
+    private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
@@ -37,7 +39,7 @@ public class QuizActivity extends AppCompatActivity {
     //Keeps track of the current question being displayed
     private int mCurrentIndex = 0;
 
-
+    private boolean[] mIsCheater = new boolean[mQuestionBank.length];   //An array that keeps track of which questions you cheated on.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +50,9 @@ public class QuizActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_quiz);
 
-        if(savedInstanceState != null)  {
+        if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater[mCurrentIndex] = savedInstanceState.getBoolean(KEY_CHEAT, false);
         }
 
         //Grabs a reference to the question text box
@@ -89,7 +92,7 @@ public class QuizActivity extends AppCompatActivity {
 
         //Grabs reference to the Next Button then sets a listener for user click input.
         mNextButton = (Button) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener()   {
+        mNextButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -111,43 +114,102 @@ public class QuizActivity extends AppCompatActivity {
                 // Start CheatActivity
                 boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
                 Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+
+            mIsCheater[mCurrentIndex] = CheatActivity.wasAnswerShown(data);
+        }
+    }
+
+
+    //Saves mCurrentState in Bundle (set of key-value-pairs) so when screen rotates app doesnt reset.
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBoolean(KEY_CHEAT, mIsCheater[mCurrentIndex]);
+    }
+
+
+    //Private method which updates the question text in the app
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    //Private method which checks the users answer against the correct answer stored in the Question Model.
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (mIsCheater[mCurrentIndex] == true) {
+            messageResId = R.string.judgment_toast;
+        } else {
+
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+
+                //Increments score if answer is correct.
+                mScore++;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
+
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    //Keeps track of then displays the final score.
+    private void finalScore() {
+
+        String answer = mScore + "/" + mQuestionBank.length;
+
+        if (mCurrentIndex == 0) {
+            Toast.makeText(this, answer, Toast.LENGTH_SHORT).show();
+            mScore = 0;
+            Log.d(TAG, "Quiz was completed.");
+        }
+    }
+
     //Logs activity of app when switching states.
     @Override
-    public void onStart()   {
+    public void onStart() {
         super.onStart();
         Log.d(TAG, "onStart() called");
     }
 
     //Logs activity of app when switching states.
     @Override
-    public void onResume()  {
+    public void onResume() {
         super.onResume();
         Log.d(TAG, "onResume() called");
     }
 
     //Logs activity of app when switching states.
     @Override
-    public void onPause()   {
+    public void onPause() {
         super.onPause();
         Log.d(TAG, "onPause() called");
     }
 
-    //Saves mCurrentState in Bundle (set of key-value-pairs) so when screen rotates app doesnt reset.
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState)  {
-        super.onSaveInstanceState(savedInstanceState);
-        Log.i(TAG, "onSaveInstanceState");
-        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
-    }
-
     //Logs activity of app when switching states.
     @Override
-    public void onStop()    {
+    public void onStop() {
         super.onStop();
         Log.d(TAG, "onStop() called");
     }
@@ -157,40 +219,5 @@ public class QuizActivity extends AppCompatActivity {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy() called");
-    }
-
-    //Private method which updates the question text in the app
-    private void updateQuestion()   {
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
-    }
-
-    //Private method which checks the users answer against the correct answer stored in the Question Model.
-    private void checkAnswer(boolean userPressedTrue)   {
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-
-        int messageResId = 0;
-
-        if(userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
-
-            //Increments score if answer is correct.
-            mScore++;
-        } else  {
-            messageResId = R.string.incorrect_toast;
-        }
-
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
-    }
-
-    private void finalScore() {
-
-        String answer = mScore + "/" + mQuestionBank.length;
-
-        if(mCurrentIndex == 0)   {
-            Toast.makeText(this, answer, Toast.LENGTH_SHORT).show();
-            mScore = 0;
-            Log.d(TAG, "Quiz was completed.");
-        }
     }
 }
